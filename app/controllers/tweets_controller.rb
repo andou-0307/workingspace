d1978e99ae8d73750814164b168f95e6e0ea38f2
andou-0307 <703515412307a@gmail.com>
2020-08-28 16:29:15 +0900

    コントローラー

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,23 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def upadte
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+  
 end
