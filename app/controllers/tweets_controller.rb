2d8c491e1d7034e81b045719aba217ddbbcf6dbb
andou-0307 <703515412307a@gmail.com>
2020-09-11 15:11:14 +0900

    【修正】必要な部分のみ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,23 +3,5 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def upadte
-  end
-
-  def destroy
-  end
-
-  def show
-  end
   
 end
